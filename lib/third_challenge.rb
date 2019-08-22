37f74947cd8870f62447575516c3224127e78d81
Priscilla Firpo <priscillayf@gmail.com>
2019-08-22 22:14:20 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,8 +3,8 @@ def third
      :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-       :hero => {name: "Romeo", age: "15", status: "alive"},
-       :hero_friends => []
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => []
      },
      :capulet => {
        :patriarch => {name: "Lord Capulet", age: "50"},
